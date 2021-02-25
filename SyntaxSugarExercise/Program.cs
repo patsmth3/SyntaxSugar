11a3a0c1a72d08b69e0b2a1c9baee27450076ab2
Pat Smith <patsmith@gmail.com>
2021-02-24 21:21:15 -0500

    initial commit

---
+++ SyntaxSugarExercise/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SyntaxSugarExercise
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var answer = 4;
+            //var response;
+
+            /*if (answer < 9)
+            {
+                response = answer + " is less than nine";
+            }
+            else
+            {
+                response = answer + "greater than or equal to nine";
+            }
+            Console.WriteLine(response);
+            */
+
+            // -----------String Ternary---------------------------
+
+            var response = (answer < 9) ? "is less than nine" : "greater than or equal to nine";
+            Console.WriteLine($"{response} {answer}");
+
+
+
+        }
+    }
+}
